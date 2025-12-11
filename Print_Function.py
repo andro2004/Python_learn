376f983d09e0a6d772e3796a6c4d2b068ce81108
Andro <Andro_milad22@yahoo.com>
2025-12-11 23:54:03 +0200

    problem solved

---
+++ Print_Function.py
@@ -0,0 +1,14 @@
+#The included code stub will read an integer, , from STDIN.
+#Without using any string methods, try to print the following:
+#Note that "" represents the consecutive values in between.
+#Example input = 5
+#Print the string 12345.
+
+#####solution:
+
+if __name__ == '__main__':
+    n = int(input())
+
+
+for i in range(n):
+    print(i+1,end="")
