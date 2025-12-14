8a8b9375f127fa99b971cfa9eeacaf6d9feefce9
Andro <Andro_milad22@yahoo.com>
2025-12-14 12:00:48 +0200

    new changes

--- List_Comprehensions.py
+++ List_Comprehensions.py
@@ -4,21 +4,11 @@
 
 #####solution:
 if __name__ == '__main__':
-    print ("Enter x:")
     x = int(input())
-    print ("Enter y:")
     y = int(input())
-    print ("Enter z:")
     z = int(input())
-    print ("Enter n:")
     n = int(input())
     
-#    temp = [x,y,z]
-    coordinates =[]
-    for i in range(x+1):
-        for j in range(y+1):
-            for k in range(z+1):
-                if (i+j+k)!=n:
-                    coordinates.append([i,j,k])
+    coordinates =[[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k)!=n]
 
     print(coordinates)
